a7e72be3645f21e6cf41a10ddbc74ff4fe8ee0b3
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-24 18:33:35 +1000

    var declarations and scopes

--- 3. JavaScript/Variable Declarations/index.js
+++ 3. JavaScript/Variable Declarations/index.js
@@ -1,2 +1,28 @@
-
 // var declaration
+
+var fullName = "Ajay";
+
+if (fullName === "Ajay") {
+  var fullName = "Ajita";
+  console.log(fullName); // "Ajita"
+}
+console.log(fullName); // "Ajita"
+
+
+var x = 1;
+function foo() {
+
+  function bar() {
+    var x = 3;
+    var y = 2;
+    console.log(x); // 1 (function `bar` closes over `x`)
+    console.log(y); // 2 (`y` is in scope)
+  }
+  bar();
+  console.log(x); // 1 (`x` is in scope)
+  console.log(y); // ReferenceError, `y` is scoped to `bar`
+}
+
+foo();
+
+
