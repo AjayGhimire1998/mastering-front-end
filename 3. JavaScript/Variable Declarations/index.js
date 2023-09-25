2d1faf1b8dd1da219621d33ce27cda7d6cf15582
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-25 19:42:54 +1000

    variable declaration

--- 3. JavaScript/Variable Declarations/index.js
+++ 3. JavaScript/Variable Declarations/index.js
@@ -1,28 +1,62 @@
 // var declaration
+// var fullName = "Ajay";
 
-var fullName = "Ajay";
+// if (fullName === "Ajay") {
+//   var fullName = "Ajita";
+//   console.log(fullName); // "Ajita"
+// }
+// console.log(fullName); // "Ajita"
 
-if (fullName === "Ajay") {
-  var fullName = "Ajita";
-  console.log(fullName); // "Ajita"
-}
-console.log(fullName); // "Ajita"
+// var x = 1;
+// function foo() {
+//   var x = 2;
+//   function bar() {
+//     var x = 3;
+//     var y = 4;
+//     console.log(x); // 3 ('x' is in scope)
+//     console.log(y); // 4 (`y` is in scope)
+//   }
+//   bar();
+//   console.log(x); // 2 (`x` is in scope)
+//   console.log(y); // ReferenceError, `y` is scoped to `bar`
+// }
+// foo();
+// console.log(x); // 1 ('x' is in global scope)
 
+// let declaration
+// function foo() {
+//   let x = 1;
+//   function bar() {
+//     let x = 2;
+//     let y = 3;
+//     console.log(x); // 2 ('x' is in scope)
+//     console.log(y); // 3 (`y` is in scope)
+//   }
+//   bar();
+//   console.log(x); // 1 (`x` is in scope)
+//   console.log(y); // ReferenceError, `y` is scoped to `bar`
+// }
+// foo();
+// console.log(x); // ReferenceError: x is not defined
+// console.log(y); // ReferenceError: y is not defined
 
-var x = 1;
+// const declaration
 function foo() {
-
+  const x = 1;
   function bar() {
-    var x = 3;
-    var y = 2;
-    console.log(x); // 1 (function `bar` closes over `x`)
-    console.log(y); // 2 (`y` is in scope)
+    const x = 2;
+    const y = 3;
+    console.log(x); // 2 ('x' is in scope)
+    console.log(y); // 3 (`y` is in scope)
   }
   bar();
   console.log(x); // 1 (`x` is in scope)
   console.log(y); // ReferenceError, `y` is scoped to `bar`
 }
-
 foo();
+console.log(x); // ReferenceError: x is not defined
+console.log(y); // ReferenceError: y is not defined
 
-
+const z = 10;
+z = 20;
+console.log(z); // TypeError: Assignment to constant variable.
