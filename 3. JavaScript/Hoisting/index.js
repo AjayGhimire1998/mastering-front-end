a0de7a6af830a30797068f8a31cb30126481cf6e
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-26 16:17:50 +1000

    hoisting done

--- 3. JavaScript/Hoisting/index.js
+++ 3. JavaScript/Hoisting/index.js
@@ -1,30 +1,42 @@
-//variable hoisting
-// console.log(greeting); // ReferenceError:  Cannot access 'greeting' before initialization
-// let greeting = "Hello!!";
+/* variable hoisting */
 
-// let greeting_two;
-// console.log(greeting_two); //undefined
-// greeting_two = "Hyy!!";
+console.log(greeting); // ReferenceError:  Cannot access 'greeting' before initialization
+let greeting = "Hello!!";
 
-// console.log(PI); //ReferenceError: Cannot access 'PI' before initialization
-// const PI = 3.14;
+let greeting_two;
+console.log(greeting_two); //undefined
+greeting_two = "Hyy!!";
 
-//functions hoistng
+console.log(PI); //ReferenceError: Cannot access 'PI' before initialization
+const PI = 3.14;
+
+/*functions hoistng */
+
 console.log(greet()); // Hello!!
 function greet() {
   return "Hello!!";
 }
-
-// function sayHelloToAjay() {
-//   //   console.log(who);
-//   who = "Hello" + AJAY;
-//   console.log(AJAY);
-//   const AJAY = "AJAY";
-// }
-// sayHelloToAjay(); //ReferenceError: who is not defined
-
+function sayHelloToAjay() {
+  console.log(who);
+  who = "Hello" + AJAY;
+  console.log(AJAY);
+  const AJAY = "AJAY";
+}
+sayHelloToAjay(); //ReferenceError: who is not defined
 
 sayHelloToAjayTwo(); // ReferenceError: Cannot access 'sayHelloToAjayTwo' before initialization
 let sayHelloToAjayTwo = function () {
   console.log("Hello AJAY");
 };
+
+var square;
+function square(num) {
+  return num * num;
+}
+console.log(typeof square); //function
+
+fun(); // TypeError: fun is not a function
+var fun = () => console.log("FUNN");
+
+fun(); // ReferenceError: Cannot access 'fun' before initialization
+let fun = () => console.log("FUNN");
