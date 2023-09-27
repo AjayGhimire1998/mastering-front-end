459b31c3881dd33da243ada5020ceb4ca64248e4
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-27 13:13:05 +1000

    more inheritance in js

--- 3. JavaScript/Object-Prototype/index.js
+++ 3. JavaScript/Object-Prototype/index.js
@@ -1,11 +1,34 @@
+/* Object Prototype*/
 const person = {
-    name: "Ajay",
-    age: 25,
-    getAge: function() {
-        return this.age;
-    }
-}
-console.log(person.__proto__); //[Object: null prototype] {}
-console.log(Object.getPrototypeOf(person)); //[Object: null prototype] {}
+  name: "Ajay",
+  age: 25,
+  getAge: function () {
+    return this.age;
+  },
+};
+// console.log(person.__proto__); //[Object: null prototype] {}
+// console.log(Object.getPrototypeOf(person)); //[Object: null prototype] {}
 
-console.log(person.__proto__ === Object.getPrototypeOf(person)); //true
+// console.log(person.__proto__ === Object.getPrototypeOf(person)); //true
+
+//Dog class
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+    this.bark = function() {
+        console.log("woof woof");
+    }
+  }
+}
+// Dog.prototype.bark = function (sound) {
+//   console.log(`${this.name} says ${sound}`);
+// };
+const dog = new Dog("Tommy", "Labrador");
+const dog2 = new Dog("Kutta", "Husky");
+console.log(dog);
+console.log(dog2);
+console.log(dog.bark === dog2.bark); //false because they are two diffrenet properties in the memory
+dog.bark();
+dog2.bark();
+// console.log(Dog.prototype);
