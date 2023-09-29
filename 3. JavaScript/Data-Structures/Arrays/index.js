f5eab4db1e25cff87dc7a5d918e1b05f4ae30bf8
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-29 15:49:45 +1000

    Arrays in JS

---
+++ 3. JavaScript/Data-Structures/Arrays/index.js
@@ -0,0 +1,14 @@
+const names = ["Ajay", "Arjun", "Sia", "Mama"];
+console.log(names);
+names[10] = "hehe"; // leaves index 4 to 9 empty
+console.log(names);
+
+const whatever = [
+  "whatever",
+  1,
+  "it",
+  "takes",
+  { name: "ajay", age: 24 },
+  true,
+]; //dynamic and can hold mixed types
+console.log(whatever);
