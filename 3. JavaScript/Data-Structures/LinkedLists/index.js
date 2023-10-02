d40858fda7804c4e57a97e96b5c643479fc7755b
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-02 19:03:11 +1100

    linkedlist

---
+++ 3. JavaScript/Data-Structures/LinkedLists/index.js
@@ -0,0 +1,15 @@
+/* 
+Linked lists are a type of data structure 
+that store values in the form of a list. 
+Within the list, each value is considered a node, 
+and each node is connected with the following value 
+in the list (or null in case the element is the last in the list) through a pointer.
+
+There are two kinds of linked lists, 
+singly linked lists and doubly linked lists.
+ Both work very similarly, 
+ but the difference is in singly linked lists 
+ each node has a single pointer that indicates the next node on the list. 
+ While in doubly linked lists, each node has two pointers, 
+ one pointing to the next node and another pointing to the previous node.
+ */
