fe3dd346994cae32297c3779496d4349e57b3a2b
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-02 19:50:29 +1100

    added insertLast method for the Singly Linked List

--- 3. JavaScript/Data-Structures/LinkedLists/index.js
+++ 3. JavaScript/Data-Structures/LinkedLists/index.js
@@ -13,3 +13,46 @@ singly linked lists and doubly linked lists.
  While in doubly linked lists, each node has two pointers, 
  one pointing to the next node and another pointing to the previous node.
  */
+
+//Singly LinkedList
+// We create a class for each node within the list
+class SinglyLinkedListNode {
+  // Each node has three properties, its value, a pointer that indicates the node that follows and a pointer that indicates the previous node
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+// We create a class for the list
+class SinglyLinkedList{
+    // The list has three properties, the head, the tail and the list size
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    // The insertLast method takes a value as parameter and assigns it as the tail of the list
+    insertLast(value){
+        const newNode = new SinglyLinkedListNode(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    removeLast(){
+    
+    }
+}
+
+const list = new SinglyLinkedList();
+list.insertLast(1);
+console.log(list);
+
