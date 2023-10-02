6a118e5ce7ff1962f94eb113ed5d88b716b078aa
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-02 18:31:12 +1100

    Added push method for stack

--- 3. JavaScript/Data-Structures/Stacks/index.js
+++ 3. JavaScript/Data-Structures/Stacks/index.js
@@ -13,7 +13,7 @@ Last in, first out. LIFO. 😉 */
 //Custom:
 
 // We create a class for each node within the stack
-class Node {
+class StackNode {
   // Each node has two properties, its value and a pointer that indicates the node that follows
   constructor(value) {
     this.value = value;
@@ -25,6 +25,29 @@ class Node {
 class Stack {
   // The stack has three properties, the first node, the last node and the stack size
   constructor() {
-    
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // The push method receives a value and adds it to the "top" of the stack
+  push(val) {
+    let newNode = new StackNode(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let tempNode = this.first;
+      this.first = newNode;
+      this.first.next = tempNode;
+    }
+    return this.size++;
   }
 }
+
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.first);
