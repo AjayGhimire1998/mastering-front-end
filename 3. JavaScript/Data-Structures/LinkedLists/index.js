387d434a4a73a40ba0c1a678db0528221fa1facc
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-04 08:42:16 +1100

    commited linkedlist

--- 3. JavaScript/Data-Structures/LinkedLists/index.js
+++ 3. JavaScript/Data-Structures/LinkedLists/index.js
@@ -343,3 +343,8 @@ class DoublyLinkedList {
     return true;
   }
 }
+/* The big O of doubly linked lists methods is the following:
+Insertion - O(1)
+Removal - O(1)
+Search - O(n)
+Access - O(n) */
