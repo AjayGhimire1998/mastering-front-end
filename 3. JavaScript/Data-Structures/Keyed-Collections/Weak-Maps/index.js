47f769104881bf5c85f56d34e42e1bdfafbeaa9a
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-08 16:13:17 +1100

    weak map

---
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Weak-Maps/index.js
@@ -0,0 +1,15 @@
+/* The first difference between Map and WeakMap 
+ is that keys must be objects, not primitive values: */
+
+let weakMap = new WeakMap();
+let obj = { test: "test obj" };
+weakMap.set(obj, "ok");
+// weakMap.set("test", "not ok"); //TypeError: Invalid value used as weak map key
+console.log(weakMap.get(obj)); //ok
+
+/* Now, if we use an object as the key in it,
+ and there are no other references to that object –
+ it will be removed from memory (and from the map) automatically. */
+
+obj = null;
+console.log(weakMap.get(obj)); //undefined
