883386c89da0766121386f129597d1a397690ec9
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-08 14:32:03 +1100

    maps

---
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Maps/index.js
@@ -0,0 +1,8 @@
+/* 
+Map is a collection of key-value pairs where the key can be of any type. 
+Map remembers the original order in which the elements were added to it, 
+which means data can be retrieved in the same order it was inserted in.
+
+In other words, Map has characteristics of both Object and Array:
+Like an object, it supports the key-value pair structure.
+Like an array, it remembers the insertion order. */
