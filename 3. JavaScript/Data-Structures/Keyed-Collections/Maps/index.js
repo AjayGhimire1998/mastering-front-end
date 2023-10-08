17351fba515a66b47817953fa9ae012390f3b081
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-08 14:42:15 +1100

    keys types in maps and objects

--- 3. JavaScript/Data-Structures/Keyed-Collections/Maps/index.js
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Maps/index.js
@@ -26,3 +26,22 @@ newMap.set("age", 16);
 newMap.set("type", "Person");
 newMap.set("age", 25); // duplicate key replaces the previous value
 console.log(newMap);
+
+//getting value from the map
+const age = newMap.get("age");
+console.log(age); //25
+
+//keys can be of any types
+newMap.set(99, "IQ Score"); //number as key
+newMap.set(true, "Is Male ?"); //boolean as key
+newMap.set({ hobbies: ["Writing Code, Walking, Listening to Music"] }, true); //object as key
+console.log(newMap);
+
+/* A regular JavaScript object always treats the key as a string. 
+Even when you pass it a primitive or object, 
+it internally converts the key into a string: */
+const funObj = {};
+funObj[360] = "Angle of a circle"
+console.log(funObj); //{ '360': 'Angle of a circle' }
+console.log(funObj[360] === funObj['360']); //true
+console.log(typeof funObj[360]) //string
