8f9a778b07b743d516c1f22fd37c28a0930dfa99
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-10-08 14:34:39 +1100

    creating and initialising map

--- 3. JavaScript/Data-Structures/Keyed-Collections/Maps/index.js
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Maps/index.js
@@ -6,3 +6,15 @@ which means data can be retrieved in the same order it was inserted in.
 In other words, Map has characteristics of both Object and Array:
 Like an object, it supports the key-value pair structure.
 Like an array, it remembers the insertion order. */
+
+//creating map
+const map = new Map();
+console.log(map); //Map(0){}
+
+//initailising map
+const infoMap = new Map([
+  ["name", "Ajay"],
+  ["age", 16],
+  ["type", "Person"],
+]);
+console.log(infoMap);
