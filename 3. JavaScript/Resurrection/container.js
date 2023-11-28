71b551a6162ddbea94703aa41784d57cb856c6b2
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-29 10:06:33 +1100

    numberic explciit

--- 3. JavaScript/Resurrection/container.js
+++ 3. JavaScript/Resurrection/container.js
@@ -23,12 +23,28 @@
 //type casting/conversions/coercions
 
 //implicit 
-// let isTrue = {} // {}, [], all other with values gives true, while 0, "", null, undefined gives false
 
+// let isTrue = {} // {}, [], all other with values gives true, while 0, "", null, undefined gives false
 // console.log(isTrue ? "It is true" : "It is false");
 
+//numeric implicit
+// console.log("10" / 2);
+
+// console.log( Number("Sup")); //Nan
+// console.log(typeof Number("Sup")); //number
+
+console.log(Number(undefined)); //Nan
+console.log(Number(null)); //0
+console.log(Number(true)); //1
+
+
+
 //explicit
-let val = 2;
-console.log(val);
-val = String(val);
-console.log(typeof val);
+// let val = 2;
+// console.log(val); // 2 (number)
+// val = String(val);
+// console.log(typeof val); //stirng
+
+// console.log(String(false)); //false
+// console.log(String(undefined)); //undefined
+
