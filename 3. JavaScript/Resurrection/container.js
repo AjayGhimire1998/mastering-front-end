2f551791dfa5c7f7172eae63313f63375f744559
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-29 09:48:19 +1100

    dumping old concs

--- 3. JavaScript/Resurrection/container.js
+++ 3. JavaScript/Resurrection/container.js
@@ -4,3 +4,13 @@
 let message;
 message = "POP!"
 console.log(message);
+
+
+//typeof
+console.log(typeof new String("hello")); //obj
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //obj
+console.log(typeof 5861658179587126578218756596859236568n); 
+
+console.log( null === null); //true
