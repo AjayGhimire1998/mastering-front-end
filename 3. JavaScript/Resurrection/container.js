138ca7d38711a2ecd4e61447ce7bfb7fdb1c4363
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-29 09:59:31 +1100

    explicit type cast

--- 3. JavaScript/Resurrection/container.js
+++ 3. JavaScript/Resurrection/container.js
@@ -1,28 +1,34 @@
 
 
-// dec var
-let message;
-message = "POP!"
-console.log(message);
+// // dec var
+// let message;
+// message = "POP!"
+// console.log(message);
 
 
-//typeof
-console.log(typeof new String("hello")); //obj
-console.log(typeof undefined); //undefined
+// //typeof
+// console.log(typeof new String("hello")); //obj
+// console.log(typeof undefined); //undefined
 
-console.log(typeof null); //obj
-console.log(typeof 5861658179587126578218756596859236568n); 
+// console.log(typeof null); //obj
+// console.log(typeof 5861658179587126578218756596859236568n); 
 
-console.log( null === null); //true'
-console.log( typeof console); //obj
-console.log(typeof alert); //undefined
-console.log(typeof log); //undefined
-console.log(typeof(2 + "a")); //string
+// console.log( null === null); //true'
+// console.log( typeof console); //obj
+// console.log(typeof alert); //undefined
+// console.log(typeof log); //undefined
+// console.log(typeof(2 + "a")); //string
 
 
 //type casting/conversions/coercions
 
 //implicit 
-let isTrue = {} // {}, [], all other with values gives true, while 0, "", null, undefined gives false
+// let isTrue = {} // {}, [], all other with values gives true, while 0, "", null, undefined gives false
 
-console.log(isTrue ? "It is true" : "It is false");
+// console.log(isTrue ? "It is true" : "It is false");
+
+//explicit
+let val = 2;
+console.log(val);
+val = String(val);
+console.log(typeof val);
