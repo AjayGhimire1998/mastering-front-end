64dec7738070c5fb5738ce26e9107f402b3d23fd
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-29 14:09:44 +1100

    equality algorithms

--- 3. JavaScript/Equality Comparison & Sameness/index.js
+++ 3. JavaScript/Equality Comparison & Sameness/index.js
@@ -2,3 +2,19 @@
 // == vs === and =! vs ==!
 console.log(10 == "10"); //true because type conversion occcurs before the comparison
 console.log(10 === "10"); //false because it strongly checks the value
+console.log( 10 != "10"); //false
+console.log( null === null);  //true
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+
+let name1 = {name: "Ajay"};
+let name2 = {name: "Ajay"};
+console.log(name1 == name2); //false
+console.log(name1 === name2); //false
+
+
+console.log(null === null); //true
+console.log(NaN === NaN); //false
+
+
+// equality algorithms
