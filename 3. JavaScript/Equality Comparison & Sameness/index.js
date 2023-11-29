5887b9e0d0d2b303ac472f57418f0d7e683b64be
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-29 13:46:37 +1100

    equality == vs ===

---
+++ 3. JavaScript/Equality Comparison & Sameness/index.js
@@ -0,0 +1,4 @@
+
+// == vs === and =! vs ==!
+console.log(10 == "10"); //true because type conversion occcurs before the comparison
+console.log(10 === "10"); //false because it strongly checks the value
