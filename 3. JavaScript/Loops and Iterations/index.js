0fb9ca7c5924cde72181fa415e0668268c272100
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-30 20:39:54 +1100

    for loops

--- 3. JavaScript/Loops and Iterations/index.js
+++ 3. JavaScript/Loops and Iterations/index.js
@@ -1,5 +1,10 @@
-
 //for-loop
-for(let i = 0; i < 10; i++){
-    console.log(i+1 + ". " + "Ajay");
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1 + ". " + "Ajay");
 }
+
+let count = 0;
+for (count = 0; count < 5; count++) {
+  console.log("count inside: " + count); // 0, 1, 2, 3, 4
+}
+console.log("count outside: " + count); //5
