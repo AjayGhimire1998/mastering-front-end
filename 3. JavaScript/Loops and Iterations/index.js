9ac3491c250c68ffade19aecd526acc676111466
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-30 20:49:26 +1100

    adding html using loops

--- 3. JavaScript/Loops and Iterations/index.js
+++ 3. JavaScript/Loops and Iterations/index.js
@@ -1,10 +1,19 @@
 //for-loop
-for (let i = 0; i < 10; i++) {
-  console.log(i + 1 + ". " + "Ajay");
+// for (let i = 0; i < 10; i++) {
+//   console.log(i + 1 + ". " + "Ajay");
+// }
+
+// let count = 0;
+// for (count = 0; count < 5; count++) {
+//   console.log("count inside: " + count); // 0, 1, 2, 3, 4
+// }
+// console.log("count outside: " + count); //5
+
+let child = `<button class="button">Hello</button>`
+let counter = 0; 
+for(counter = 0; counter < 5; counter++){
+    document.body.innerHTML += child;
+    let button = document.body.getElementsByClassName("button");
+    
 }
 
-let count = 0;
-for (count = 0; count < 5; count++) {
-  console.log("count inside: " + count); // 0, 1, 2, 3, 4
-}
-console.log("count outside: " + count); //5
