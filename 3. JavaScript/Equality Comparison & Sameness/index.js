f878332ff1279d511fdb27b2e36051f73fc8e4da
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-11-30 20:33:26 +1100

    loops and iterations

--- 3. JavaScript/Equality Comparison & Sameness/index.js
+++ 3. JavaScript/Equality Comparison & Sameness/index.js
@@ -1,22 +1,35 @@
 
 // == vs === and =! vs ==!
-console.log(10 == "10"); //true because type conversion occcurs before the comparison
-console.log(10 === "10"); //false because it strongly checks the value
-console.log( 10 != "10"); //false
-console.log( null === null);  //true
-console.log(null == undefined); //true
-console.log(null === undefined); //false
+// console.log(10 == "10"); //true because type conversion occcurs before the comparison
+// console.log(10 === "10"); //false because it strongly checks the value
+// console.log( 10 != "10"); //false
+// console.log( null === null);  //true
+// console.log(null == undefined); //true
+// console.log(undefined == undefined);
+// console.log(null === undefined); //false
 
-let name1 = {name: "Ajay"};
-let name2 = {name: "Ajay"};
-console.log(name1 == name2); //false
-console.log(name1 === name2); //false
-
-
-console.log(null === null); //true
-console.log(NaN === NaN); //false
-console.log(10 === 10);
-console.log(0 === -0); //true
+// let name1 = {name: "Ajay"};
+// let name2 = {name: "Ajay"};
+// console.log(name1 == name2); //false
+// console.log(name1 === name2); //false
 
 
-// equality algorithms
+// console.log(null === null); //true
+// console.log(NaN === NaN); //false
+// console.log(10 === 10);
+// console.log(0 === -0); //true
+
+
+// basic operatos, maths
+
+let x = "1"; 
+x = -x;
+console.log(x);
+
+let y = -2;
+let z = +y;
+console.log(z);
+
+let apples = "2";
+let oranges = "3";
+console.log(+apples + +oranges);
