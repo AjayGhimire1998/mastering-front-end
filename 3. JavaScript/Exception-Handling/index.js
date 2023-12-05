a880ce24d37da341bf54bca56064a48bfb8f81f5
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-05 20:11:37 +1100

    custome exception handling

--- 3. JavaScript/Exception-Handling/index.js
+++ 3. JavaScript/Exception-Handling/index.js
@@ -1,2 +1,18 @@
-
 //throwing exception
+// let i = "";
+// if (!!i) {
+//   console.log(i);
+// } else {
+//   throw new Error("Error laading this script");
+// }
+
+
+//error.message, error.code
+
+function CustomException(message) {
+    const error = new Error(message);
+    error.code = 'THIS_IS_A_CUSTOM_ERROR_CODE';
+    return error
+}
+
+console.log(CustomException("Cannot load.").message); //cannot load.
