d7c49398685ad805c1a3d8296d314104ec758fa7
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-07 20:48:30 +1100

    this keyword

--- 3. JavaScript/This-keyword/index.js
+++ 3. JavaScript/This-keyword/index.js
@@ -19,18 +19,43 @@
 
 //this in event
 
-var button = {
-    innerHTML: "Click Me", // Set button text
-    id: "myButton", // Set button ID
-    addEventListener: function(event, callback) {
-      console.log("Event listener added for", event);
-      // Simulating an event listener
-      callback();
-    }
-  };
+// var button = {
+//     innerHTML: "Click Me", // Set button text
+//     id: "myButton", // Set button ID
+//     addEventListener: function(event, callback) {
+//       console.log("Event listener added for", event);
+//       // Simulating an event listener
+//       callback();
+//     }
+//   };
 
-button.addEventListener('click', function() {
-    console.log(this.innerHTML);
+// button.addEventListener('click', function() {
+//     console.log(this.innerHTML);
 
-})
+// })
 
+
+//this in obj method refers to the obj
+
+// const person = { 
+//     firstName: "Ajay",
+//     lastName: "Ghimire",
+//     getName(){
+//         return this.firstName + this.lastName;
+//     }
+// }
+
+// console.log(person.getName());
+
+//tthis binding
+
+function func () {
+    return this;
+}
+const obj1 = {name: "obj1"}
+
+obj1.func = func;
+
+console.log(obj1.func());
+
+// console.log(func());
