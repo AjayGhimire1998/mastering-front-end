787350ea6eaf975d597647e6764cc0fb7aa26adc
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-07 20:37:58 +1100

    document inside the node env

--- 3. JavaScript/This-keyword/index.js
+++ 3. JavaScript/This-keyword/index.js
@@ -9,10 +9,28 @@
 // sayHello();  //global obj
 
 
-"use strict"
+// "use strict"
 
-function sayHello(){
-    console.log(this); 
-}
-sayHello(); //unddefined
+// function sayHello(){
+//     console.log(this); 
+// }
+// sayHello(); //unddefined
+
+
+//this in event
+
+var button = {
+    innerHTML: "Click Me", // Set button text
+    id: "myButton", // Set button ID
+    addEventListener: function(event, callback) {
+      console.log("Event listener added for", event);
+      // Simulating an event listener
+      callback();
+    }
+  };
+
+button.addEventListener('click', function() {
+    console.log(this.innerHTML);
+
+})
 
