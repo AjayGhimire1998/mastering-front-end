67442a40f505b544c492ed1be81e2a5054e74be1
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-07 20:29:44 +1100

    this refers to global in func, and undefined in strict func

--- 3. JavaScript/This-keyword/index.js
+++ 3. JavaScript/This-keyword/index.js
@@ -1,1 +1,18 @@
 //this kleyword
+
+// console.log(this);
+
+
+// function sayHello(){
+//     console.log(this);
+// }
+// sayHello();  //global obj
+
+
+"use strict"
+
+function sayHello(){
+    console.log(this); 
+}
+sayHello(); //unddefined
+
