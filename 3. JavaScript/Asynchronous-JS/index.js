f1619cb97ef95a4f09e1a2ab847dc44f9c65eb52
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-08 19:53:04 +1100

    generating primes

--- 3. JavaScript/Asynchronous-JS/index.js
+++ 3. JavaScript/Asynchronous-JS/index.js
@@ -5,12 +5,24 @@
 const MAX_PRIME = 1_000_000;
 
 function isPrime(n){
-    for(let i = 2; i <= Math.sqrt(n); i++){
-        if (n % i ===0) {
-            return false
-        }
+   for(let i = 2; i <= Math.sqrt(n); i++ ){
+    if (n % i === 0) {
+        return false;
     }
-    return n>1;
+   }
+   return n > 1;
 }
 
-console.log(isPrime(23244));
+function generatePrimeInrange(start, end){
+    const primes = [];
+
+    for(let num = Math.max(2, start); num <= end; num++){
+        if (isPrime(num)){
+            primes.push(num)
+        }
+    }
+    return primes;
+}
+
+console.log(isPrime(119));
+console.log(generatePrimeInrange(1,100));
