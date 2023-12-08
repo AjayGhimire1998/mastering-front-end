0556cd752596846245cc69524b6267cbc3663542
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-08 14:21:10 +1100

    call binding

--- 3. JavaScript/This-keyword/index.js
+++ 3. JavaScript/This-keyword/index.js
@@ -104,3 +104,15 @@
 
 
 //call
+
+function Product(name, price){
+    this.name = name;
+    this.price = price
+}
+
+function Food (name, price){
+    Product.call(this, name, price);
+    this.category = 'food';
+}
+console.log(new Food('cheese',5)); // Food {name: "cheese", price: 5, category: "food"}
+
