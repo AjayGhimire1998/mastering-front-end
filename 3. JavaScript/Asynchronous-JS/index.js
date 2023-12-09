149f1552dced7ab166c4c16df7f4aef2afe6c9db
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-09 18:03:43 +1100

    is prime revisted:

--- 3. JavaScript/Asynchronous-JS/index.js
+++ 3. JavaScript/Asynchronous-JS/index.js
@@ -2,33 +2,49 @@
 
 //synchronous js 
 
-const MAX_PRIME = 1_000_000;
+// const MAX_PRIME = 1_000_000;
 
-function isPrime(n){
-   for(let i = 2; i <= Math.sqrt(n); i++ ){
-    if (n % i === 0) {
-        return false;
-    }
-   }
-   return n > 1;
-}
+// function isPrime(n){
+//    for(let i = 2; i <= Math.sqrt(n); i++ ){
+//     if (n % i === 0) {
+//         return false;
+//     }
+//    }
+//    return n > 1;
+// }
 
-async function  generatePrimeInrange(start, end){
-    const primes = [];
+// async function  generatePrimeInrange(start, end){
+//     const primes = [];
 
-    for(let num = Math.max(2, start); num <= end; num++){
-        if (isPrime(num)){
-            primes.push(num)
+//     for(let num = Math.max(2, start); num <= end; num++){
+//         if (isPrime(num)){
+//             primes.push(num)
+//         }
+//     }
+//     return await primes;
+// }
+
+// console.log(isPrime(119));
+// console.time();
+
+// console.log(generatePrimeInrange(10,MAX_PRIME));
+// console.timeEnd();
+
+
+// console.log(Math.max(2,100));
+
+
+
+//is prime practise 
+
+
+function isPrime(num){
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) {
+            return false
         }
     }
-    return await primes;
+    return num > 1;
 }
 
-console.log(isPrime(119));
-console.time();
-
-console.log(generatePrimeInrange(10,MAX_PRIME));
-console.timeEnd();
-
-
-console.log(Math.max(2,100));
+console.log(isPrime(65));
