5ee153ad7c671bd9014a669f96e19e143e53b3a8
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-12 15:52:39 +1100

    done fetch

--- 3. JavaScript/Working-With-APIS/index.js
+++ 3. JavaScript/Working-With-APIS/index.js
@@ -3,7 +3,6 @@
 
 ///XHRHttpRequest
 
-const res = require("express/lib/response");
 
 
 
@@ -44,14 +43,56 @@ const res = require("express/lib/response");
     // console.log(commits[0].author.login);
 
 
-    fetch("https://yashitech-server.onrender.com/api/v1/services")
-  .then(response => {
-    response.json()
-    // console.log(response.headers);
-    for (let [key, value] of response.headers) {
-        console.log(`${key} = ${value}`);
-      }
-})
-  .then(commits => console.log(commits));
+//     fetch("https://yashitech-server.onrender.com/api/v1/services")
+//   .then(response => {
+//     response.json()
+//     // console.log(response.headers);
+//     for (let [key, value] of response.headers) {
+//         console.log(`${key} = ${value}`);
+//       }
+// })
+//   .then(commits => console.log(commits));
 
+/* The `alert("Hello")` statement displays a dialog box with the message "Hello". It is used to show a
+message to the user. */
+// alert("Hello")
 
+// const canvasElem = document.querySelector("#canvasElem");
+
+// canvasElem.addEventListener("mousedown", (e)  => {
+//     let ctx = canvasElem.getContext('2d');
+//     ctx.lineTo(e.clientX, e.clientY);
+//     ctx.stroke();
+//   });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const canvasElem = document.querySelector("#canvasElem");
+//     const ctx = canvasElem.getContext('2d');
+  
+//     // Set line properties
+//     ctx.lineWidth = 2;
+//     ctx.lineCap = 'round';
+//     ctx.lineJoin = 'round';
+  
+//     ctx.strokeStyle = 'black';
+//     let isDrawing = false;
+  
+//     canvasElem.addEventListener("mousedown", (e) => {
+//       isDrawing = true;
+//       ctx.beginPath();
+//       ctx.moveTo(e.clientX, e.clientY);
+//     });
+  
+//     canvasElem.addEventListener("mousemove", (e) => {
+//       if (isDrawing) {
+//         ctx.lineTo(e.clientX, e.clientY);
+//         ctx.stroke();
+//       }
+//     });
+  
+//     canvasElem.addEventListener("mouseup", () => {
+//       isDrawing = false;
+//     });
+//   });
+
+  
