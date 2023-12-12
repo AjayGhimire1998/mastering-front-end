abb910ead9e0776e14eb3e8f4efbaa901471a364
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-12 13:54:29 +1100

    trying all async prog methods to call aimate func on alice svg

--- 3. JavaScript/Asynchronous-JS/animations.js
+++ 3. JavaScript/Asynchronous-JS/animations.js
@@ -6,13 +6,38 @@ const aliceTumbling = [
 
 const aliceTiming = {
     duration: 1000,
-    iterations: 5,
-    fill: "backwards"
+    iterations: 1,
+    fill: "forwards"
 }
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-alice1.animate(aliceTumbling, aliceTiming);
-alice2.animate(aliceTumbling, aliceTiming);
-alice3.animate(aliceTumbling, aliceTiming);
+//using nested callback primises
+// alice1.animate(aliceTumbling, aliceTiming)
+//   .finished
+//   .then(() => {
+//     alice2.animate(aliceTumbling, aliceTiming)
+//       .finished
+//       .then(() => {
+//         alice3.animate(aliceTumbling, aliceTiming);
+//       });
+//   });
+
+//using promise chain
+
+// alice1.animate(aliceTumbling, aliceTiming).finished
+// .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+// .then(() => alice3.animate(aliceTumbling, aliceTiming))
+
+
+//using async/await 
+
+async function animateAlices() {
+    await alice1.animate(aliceTumbling, aliceTiming).finished
+    await alice2.animate(aliceTumbling, aliceTiming).finished
+    await alice3.animate(aliceTumbling, aliceTiming).finished
+}
+
+animateAlices()
+
