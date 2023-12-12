db418cc04e36b27dd987981d91271de286581e88
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-12 16:33:02 +1100

    making iterator

--- 3. JavaScript/Iterators/index.js
+++ 3. JavaScript/Iterators/index.js
@@ -1,6 +1,6 @@
 
 
-let nums = [1,2,3,10];
+// let nums = [1,2,3,10];
 // for(let i = 0 ; i < nums.length; i++){
 //     console.log(nums[i]);
 // }
@@ -23,3 +23,41 @@ let nums = [1,2,3,10];
 //     console.log(byte);
 //    console.log(byte.toString(2));
 // }
+
+
+//a simple iterator
+
+let nums = [1,5,167];
+
+function makeIterator( nums ) {
+//     let start = 0;
+//     let end = nums.length;
+//     const iterables = [];
+//     while (start <= end){
+//         if (start === end){
+//             iterables.push({value: undefined, done: true})
+//         } else {
+//             iterables.push({value: nums[start], done: false})
+//         }
+//         start++;
+//     }
+//   return {
+//     next() {
+        
+//     }
+//   }
+
+let count = 0;
+
+return {
+    next(){
+        if(count > nums.length - 1){
+            return {value: undefined, done: true}
+        }
+        return {value: nums[count++], done: false}
+    }
+}
+}
+let iter = makeIterator(nums);
+console.log(iter.next());
+console.log(iter.next());
