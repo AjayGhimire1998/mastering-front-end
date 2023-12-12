d0de40d57da6e7a1ad5af932a50dfa4812b81867
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-12 15:58:44 +1100

    iterator intro

---
+++ 3. JavaScript/Iterators/index.js
@@ -0,0 +1,25 @@
+
+
+let nums = [1,2,3,10];
+// for(let i = 0 ; i < nums.length; i++){
+//     console.log(nums[i]);
+// }
+
+//for..of iterator
+
+// for(let num of nums){
+//     console.log(num);
+// }
+
+// const str = "hello";
+
+// for(let char of str){
+//     console.log(char.toUpperCase());
+// }
+
+// var uint8 = new Uint8Array([15, 192, 16]);
+
+// for (var byte of uint8) {
+//     console.log(byte);
+//    console.log(byte.toString(2));
+// }
