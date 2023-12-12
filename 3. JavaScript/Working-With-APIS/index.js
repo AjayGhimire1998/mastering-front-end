3cac077472b749f752d5e1033ea966c3d3b998b9
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-12-12 15:25:11 +1100

    different fetch call methods

--- 3. JavaScript/Working-With-APIS/index.js
+++ 3. JavaScript/Working-With-APIS/index.js
@@ -30,3 +30,28 @@ const res = require("express/lib/response");
 //     let data =  response.body
 //     console.log(data);
 // }
+
+// let response =  fetch("https://yashitech-server.onrender.com/api/v1/services");
+
+//     let data = response.json()
+//     console.log(data);
+
+    // let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+    // let response =  fetch(url);
+    
+    // let commits =  response.json(); // read response body and parse as JSON
+    
+    // console.log(commits[0].author.login);
+
+
+    fetch("https://yashitech-server.onrender.com/api/v1/services")
+  .then(response => {
+    response.json()
+    // console.log(response.headers);
+    for (let [key, value] of response.headers) {
+        console.log(`${key} = ${value}`);
+      }
+})
+  .then(commits => console.log(commits));
+
+
