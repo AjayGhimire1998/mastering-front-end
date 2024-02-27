8ab3e63cd9ed40a4faee02769b8e7ff05f5cd958
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 10:58:52 +1100

    weakmap

--- 3. JavaScript/Data-Structures/Keyed-Collections/Sets/index.js
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Sets/index.js
@@ -1,16 +1,16 @@
-// /* 
-// A Set is a collection of unique elements that can be of any type. 
-// Set is also an ordered collection of elements, 
+// /*
+// A Set is a collection of unique elements that can be of any type.
+// Set is also an ordered collection of elements,
 // which means that elements will be retrieved in the same order that they were inserted in.
 
-// A Set in JavaScript behaves the same way as a mathematical set. 
+// A Set in JavaScript behaves the same way as a mathematical set.
 
-// JavaScript sets resemble arrays in the sense that they are also collections of values. 
-// But, unlike arrays, these data collections can only include unique values. 
+// JavaScript sets resemble arrays in the sense that they are also collections of values.
+// But, unlike arrays, these data collections can only include unique values.
 // In other words, you can’t have duplicates in a set.
 
-// The second difference between arrays and sets is that inside sets, 
-// the values are stored in no particular order, 
+// The second difference between arrays and sets is that inside sets,
+// the values are stored in no particular order,
 // so you can just call them by their name.
 // */
 
@@ -39,19 +39,19 @@
 // console.log(set);
 
 // /* How to Iterate Over a Set in JavaScript
-// Set has a method called values() 
+// Set has a method called values()
 // which returns a SetIterator to get all its values: */
 
 // console.log(set.values());
 
 // /* We can use a forEach or for-of loop on this to retrieve the values.
-// Interestingly, JavaScript tries to make Set compatible with Map. 
+// Interestingly, JavaScript tries to make Set compatible with Map.
 // That's why we find two of the same methods as Map, keys() and entries().
 
 // As Set doesn't have keys,
 // the keys() method returns a SetIterator to retrieve its values: */
 
-// console.log(set.entries()); //returns a entries iterator with values in arrays 
+// console.log(set.entries()); //returns a entries iterator with values in arrays
 // console.log(set.keys()); //same as set.values()
 
 // //We can enumerate over a Set using forEach and for-of loops:
@@ -64,7 +64,6 @@
 // for(const value of set){
 //     console.log(value);
 // }
-
 
 // let set = new Set();
 
@@ -92,7 +91,6 @@
 
 // console.log(set.values());
 
-
 /* Let arr be an array.
 
 Create a function unique(arr) that should return an array with unique items of arr.
@@ -112,3 +110,47 @@ P.S. Here strings are used, but can be values of any type. */
 
 // console.log(unique(values));
 
+/* Anagrams are words that have the same number of same letters, but in different order.
+
+For instance:
+
+nap - pan
+ear - are - era
+cheaters - hectares - teachers
+Write a function aclean(arr) that returns an array cleaned from anagrams.
+
+For instance:
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+From every anagram group should remain only one word, no matter which one. */
+
+// function aclean(arr) {
+//   //   for (let i = 0; i < arr.length; i++) {
+//   //     let indiArr = arr[i].split("");
+//   //     indiArr.sort();
+//   //     arr[i] = indiArr.join("").toLowerCase();
+//   //   }
+//   //   let set = new Set(arr);
+//   //   return Array.from(set)
+
+//   let map = new Map();
+
+//   for(let word of arr){
+//     let sorted = word.toLowerCase().split("").sort().join("")
+//     map.set(sorted, word);
+//   }
+//   console.log(map);
+//   return Array.from(map.values())
+// }
+
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+// console.log(aclean(arr));
+
+// let map = new Map();
+
+// map.set("name", "John");
+
+// let keys = Array.from(map.keys());
+// console.log(keys);
--- 3. JavaScript/Data-Structures/Keyed-Collections/Weak-Maps/index.js
+++ 3. JavaScript/Data-Structures/Keyed-Collections/Weak-Maps/index.js
@@ -1,27 +1,55 @@
-/* The first difference between Map and WeakMap 
- is that keys must be objects, not primitive values: */
+// /* The first difference between Map and WeakMap 
+//  is that keys must be objects, not primitive values: */
+
+// let weakMap = new WeakMap();
+// let obj = { test: "test obj" };
+// weakMap.set(obj, "ok");
+// // weakMap.set("test", "not ok"); //TypeError: Invalid value used as weak map key
+// console.log(weakMap.get(obj)); //ok
+
+// /* Now, if we use an object as the key in it,
+//  and there are no other references to that object –
+//  it will be removed from memory (and from the map) automatically. */
+
+// obj = null;
+// console.log(weakMap.get(obj)); //undefined
+
+// /* WeakMap does not support iteration and 
+// methods keys(), values(), entries(), 
+// so there’s no way to get all keys or values from it.
+
+// WeakMap has only the following methods:
+
+// weakMap.set(key, value)
+// weakMap.get(key)
+// weakMap.delete(key)
+// weakMap.has(key) */
+
+// let john = { name: "John" };
+
+// let array = [ john ];
+
+// john = null; 
+
+// console.log(john);
+
+// console.log(array);
+
+// let john = { name: "John" };
+
+// let map = new Map();
+// map.set(john, "...");
+
+// john = null; 
+// console.log(map);
+
 
 let weakMap = new WeakMap();
-let obj = { test: "test obj" };
+
+let obj = {name: "ajay"};
+
 weakMap.set(obj, "ok");
-// weakMap.set("test", "not ok"); //TypeError: Invalid value used as weak map key
-console.log(weakMap.get(obj)); //ok
 
-/* Now, if we use an object as the key in it,
- and there are no other references to that object –
- it will be removed from memory (and from the map) automatically. */
-
-obj = null;
+obj =null;
 console.log(weakMap.get(obj)); //undefined
-
-/* WeakMap does not support iteration and 
-methods keys(), values(), entries(), 
-so there’s no way to get all keys or values from it.
-
-WeakMap has only the following methods:
-
-weakMap.set(key, value)
-weakMap.get(key)
-weakMap.delete(key)
-weakMap.has(key) */
-
+ 
