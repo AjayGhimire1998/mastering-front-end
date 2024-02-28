9c8daffe391b6310a009ac8ec436bda5958b13d4
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 17:03:31 +1100

    recursion factorial

--- 3. JavaScript/Recursion and STack/index.js
+++ 3. JavaScript/Recursion and STack/index.js
@@ -40,3 +40,32 @@
 // }
 
 // console.log(sumSalaries(company));
+
+// function sumTo(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i
+//   }
+//   return sum;
+// }
+
+// function sumTo(n) {
+//   if (n == 1) {
+//     return 1;
+//   }
+//   return n + sumTo(n - 1);
+// }
+
+// function sumTo(n) {
+//   return (n * (n + 1)) / 2;
+// }
+// console.log(sumTo(10));
+
+function factorial(n){
+    if (n === 0){
+        return 1;
+    } 
+    return n * factorial(n-1);
+}
+
+console.log(factorial(4));
