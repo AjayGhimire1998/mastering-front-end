1d691c706684aefeb39a8cadac687e918877aa21
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 13:21:39 +1100

    json

---
+++ 3. JavaScript/JSON-methods/index.js
@@ -0,0 +1,23 @@
+
+// let student = {
+//     name: 'John',
+//     age: 30,
+//     isAdmin: false,
+//     courses: ['html', 'css', 'js'],
+//     spouse: null
+//   };
+
+//   console.log( JSON.stringify(student)); //string
+
+//   let json = JSON.stringify(student);
+//   console.log(JSON.parse(json));
+
+//   let user = {
+//     sayHi() { // ignored
+//       alert("Hello");
+//     },
+//     [Symbol("id")]: 123, // ignored
+//     something: undefined // ignored
+//   };
+
+//   console.log(JSON.stringify(user));
