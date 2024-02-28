9de1fea48b4e2c4cdb4ab29e701eb6755b1124b4
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 17:22:01 +1100

    fibo

--- 3. JavaScript/Recursion and STack/index.js
+++ 3. JavaScript/Recursion and STack/index.js
@@ -83,7 +83,35 @@
 //   return b;
 // }
 
-
 // console.log(fib(3)); // 2
 // console.log(fib(7)); // 13
 // console.log(fib(90));
+
+function printList(list) {
+    // let temp = list;
+    // while(temp){
+    //     console.log(temp.value);
+    //     temp = temp.next;
+    // }
+    console.log(list.value);
+    if(list.next){
+        printList(list.next);
+    }
+
+}
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+console.log(printList(list));
