2bc7d76881a247d2016bb64e22553730eb0e6eeb
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 11:38:34 +1100

    iterator methods in objects

--- 3. JavaScript/Object-Iterator-Methods/index.js
+++ 3. JavaScript/Object-Iterator-Methods/index.js
@@ -18,7 +18,6 @@
 
 // }
 
-
 // console.log(obj);
 // let obj2 = Object.assign({}, obj);
 // console.log(obj2);
@@ -27,4 +26,21 @@
 
 // for(let item of Reflect.ownKeys(obj)){
 //    console.log(item);
-// }
+// // }
+// let prices = {
+//   banana: 1,
+//   orange: 2,
+//   meat: 4,
+// };
+
+// let doublePrices = Object.fromEntries(
+//   Object.entries(prices).map((entry) => [entry[0], entry[1] * 2])
+// );
+
+// console.log(doublePrices);
+
+/* There is a salaries object with arbitrary number of salaries.
+
+Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+
+If salaries is empty, then the result must be 0. */
