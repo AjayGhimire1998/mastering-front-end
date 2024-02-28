0bc60da9daa9c422649fced84ded545be680dd61
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-28 12:21:05 +1100

    finding sum of salry

--- 3. JavaScript/Destructuring /index.js
+++ 3. JavaScript/Destructuring /index.js
@@ -1,5 +1,3 @@
-
-
 // let names = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
 
 // let [first, ,last] = names;
@@ -24,9 +22,41 @@
 // let [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
 // console.log(rest);
 
-
 // function giveSome(name, age){
 //     return `${name}: ${age}`;
 // }
 
 // console.log(giveSome(20));
+
+// let user = { name: "John", years: 30 };
+
+// let { name, years: age, isAdmin = false } = user;
+
+// console.log(name);
+// console.log(age);
+// console.log(isAdmin);
+
+// let salaries = {
+//   John: 100,
+//   Pete: 300,
+//   Ok: 200,
+//   Mary: 250,
+// };
+
+// function topSalary(salaries) {
+// //   if (Object.entries(salaries).length === 0) {
+// //     return null;
+// //   }
+
+//   let maxSalary = 0;
+//   let maxName = null;
+//   for(let [name, salary] of Object.entries(salaries)){
+//     if(maxSalary < salary){
+//         maxSalary = salary;
+//         maxName = name;
+//     }
+//   }
+//   return maxName
+// }
+
+// console.log(topSalary(salaries));
