72bba4ca03ef629db459ea02414f1a689be4c834
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-29 17:48:43 +1100

    call and bind

--- 3. JavaScript/Call & apply/index.js
+++ 3. JavaScript/Call & apply/index.js
@@ -63,3 +63,39 @@
 // }
 
 // console.log(hello());
+
+// function Animal(name, type){
+//     this.name = name;
+//     this.type = type
+//     console.log(this);
+// }
+
+// let animal = new Animal("billu", "cat");
+// console.log(animal);
+
+// class Animal {
+//     constructor(name, type) {
+//         this.name = name;
+//         this.type = type;
+//     }
+
+//     static getThis(){
+//         return this;
+//     }
+// }
+// let animal = new Animal("billu", "cat");
+// console.log(Animal.getThis());
+
+// let arr1 = ["ajay", "ghimire"];
+// let arr2 = [25, true]
+// Array.prototype.push.apply(arr1, arr2)
+// console.log(arr1);
+// console.log(arr2);
+
+function hash() {
+    let arr1 = ["ajay", "ghimire"];
+    console.log(Array.prototype.push.apply(arr1,arguments) ); // 1,2
+    console.log(arr1);
+  }
+
+  hash(1, 2)
