cb8b76214320f91499ad0c9f57451ed186ec2210
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-02-29 17:27:56 +1100

    this binding

--- 3. JavaScript/Call & apply/index.js
+++ 3. JavaScript/Call & apply/index.js
@@ -12,8 +12,54 @@
 
 //   group.showList();
 
-function slow(x) {
-  // there can be a heavy CPU-intensive job here
-  console.log(`Called with ${x}`);
-  return x;
-}
+// function slow(x) {
+//   // there can be a heavy CPU-intensive job here
+//   console.log(`Called with ${x}`);
+//   return x;
+// }
+
+// function cachingDecorator(func){
+//     let cache = new Map();
+
+   
+//     return function(x){
+//         if (cache.has(x)){
+            
+//             return cache.get(x);
+//         }
+
+//         let result = func(x); 
+//         cache.set(x, result);
+//         console.log(cache);
+//         return result;
+//     }
+// }
+
+
+// slow = cachingDecorator(slow);
+
+
+// console.log(slow(1));
+// console.log("Again: " + slow(1));
+// console.log("Again: " + slow(2));
+
+// let obj = {
+//     name: "ajay",
+//     age: 25,
+//     getObj: function(){
+//         return this;
+//     }
+// }
+
+// console.log(obj.getObj());
+
+// let res = obj.getObj.call(123);
+// console.log(-res);
+
+
+
+// function hello(){
+//     return this;
+// }
+
+// console.log(hello());
