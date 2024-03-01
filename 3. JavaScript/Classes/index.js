81c28ec310c36d5c51d49ac571f5b514fd2a725c
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-01 17:20:24 +1100

    clock

--- 3. JavaScript/Classes/index.js
+++ 3. JavaScript/Classes/index.js
@@ -62,29 +62,67 @@
 
 // user = new User(""); // Name is too short.
 
-class User {
-  name;
-  age;
+// class User {
+//   name;
+//   age;
 
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  get name() {
-    return this._name;
-  }
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   get name() {
+//     return this._name;
+//   }
 
-  set name(value) {
-    this._name = value;
-  }
+//   set name(value) {
+//     this._name = value;
+//   }
 
-  get age() {
-    return this._age;
-  }
+//   get age() {
+//     return this._age;
+//   }
 
-  set age(value) {
-    this._age = value;
-  }
-}
-let user = new User("Ajay", 25);
-console.log(user.name, user.age); // John
+//   set age(value) {
+//     this._age = value;
+//   }
+// }
+// let user = new User("Ajay", 25);
+// console.log(user.name, user.age); // John
+
+// class Clock {
+//   constructor({ template }) {
+//     this.template = template;
+//   }
+
+//   render() {
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
+
+//     let output = this.template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
+
+//     console.log(output);
+//   }
+
+//   stop() {
+//     clearInterval(this.timer);
+//   }
+
+//   start() {
+//     this.render();
+//     this.timer = setInterval(() => this.render(), 1000);
+//   }
+// }
+
+// let clock = new Clock({ template: "h:m:s" });
+// clock.start();
