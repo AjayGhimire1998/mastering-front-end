cb713b4173dae7a296dd25f39cec886d7d51a56d
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-01 17:16:25 +1100

    class

--- 3. JavaScript/Classes/index.js
+++ 3. JavaScript/Classes/index.js
@@ -31,10 +31,60 @@
 
 // let user = new User("ajay");
 
-
 // for(let prop in user){
 //     console.log(prop);
 // }
 
 // console.log(user.sayHi == User.prototype.sayHi);
 // console.log(user.sayHi());
+
+// class User {
+//   constructor(name) {
+//     // invokes the setter
+//     this.name = name;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   set name(value) {
+//     if (value.length < 4) {
+//       console.log("Name is too short.");
+//       return;
+//     }
+//     this._name = value;
+//   }
+// }
+
+// let user = new User("John");
+// console.log(user.name); // John
+
+// user = new User(""); // Name is too short.
+
+class User {
+  name;
+  age;
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    this._age = value;
+  }
+}
+let user = new User("Ajay", 25);
+console.log(user.name, user.age); // John
