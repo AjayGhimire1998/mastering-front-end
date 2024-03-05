663bae8a8e0adf6ed94445811573f3857e6fa927
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-06 09:34:33 +1100

    async itr and gens

--- 4. JavaScript/Generators/index.js
+++ 4. JavaScript/Generators/index.js
@@ -30,13 +30,13 @@
 
 //generators
 
-function* generateSeq() {
-  yield 1;
-  yield 2;
-  return 3;
-}
+// function* generateSeq() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
 
-let gen = generateSeq();
+// let gen = generateSeq();
 // console.log(gen.next());
 // console.log(gen.next());
 // console.log(gen.next());
@@ -49,29 +49,104 @@ let gen = generateSeq();
 // let seq = [0,1, ...gen]
 // console.log(seq);
 
-let range = {
-  from: 1,
-  to: 5,
+// let range = {
+//   from: 1,
+//   to: 5,
 
-  [Symbol.iterator]() {
-    return {
-      current: this.from,
-      last: this.to,
-      next() {
-        if (this.current <= this.last) {
-          return { done: false, value: this.current++ };
-        }
-        return { done: true };
-      },
-    };
-  },
-};
+//   [Symbol.iterator]() {
+//     return {
+//       current: this.from,
+//       last: this.to,
+//       next() {
+//         if (this.current <= this.last) {
+//           return { done: false, value: this.current++ };
+//         }
+//         return { done: true };
+//       },
+//     };
+//   },
+// };
 
-let itr = range[Symbol.iterator]();
-console.log(itr.next());
-console.log(itr.next());
-console.log(itr.next());
-console.log(itr.next());
-console.log(itr.next());
-console.log(itr.next());
-console.log([...range, 6, 7, 8]);
+// let itr = range[Symbol.iterator]();
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log([...range, 6, 7, 8]);
+
+// let range = {
+//   from: 1,
+//   to: 5,
+//   *[Symbol.iterator]() {
+//     for (let value = this.from; value <= this.to; value++) {
+//       yield value;
+//     }
+//   },
+// };
+// let itr = range[Symbol.iterator]();
+
+// for(let val of itr){
+//     console.log(val);
+// }
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log([...range, 6, 7, 8]);
+
+// function* generateSeq(start, end) {
+//   for (let i = start; i <= end; i++) yield i;
+// }
+
+// function* generatePasswordCodes() {
+//   // 0..9
+//   yield* generateSeq(48, 57);
+
+//   // A..Z
+//   yield* generateSeq(65, 90);
+
+//   // a..z
+//   yield* generateSeq(97, 122);
+// }
+
+// let str = "";
+
+// for (let code of generatePasswordCodes()) {
+//   str += String.fromCharCode(code);
+// }
+
+// console.log(str);
+
+// function* gen() {
+//   let result = yield "2+2 = ?";
+//   console.log(result);
+// }
+
+// let generator = gen();
+// let ques = generator.next().value;
+// // console.log(ques);
+
+// generator.next(4);
+
+// function* gen() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//   }
+  
+//   const g = gen();
+  
+//   console.log(g.next());
+//   console.log(g.return('foo'))
+//   console.log(g.next());
+
+
+//async iterables and generators
+
+
+
+
