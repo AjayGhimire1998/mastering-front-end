ad8d56f7e813f4d7a3eaae59f747a3f8e13747f7
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-06 07:54:46 +1100

    revisiting iterators

---
+++ 4. JavaScript/Generators/index.js
@@ -0,0 +1,33 @@
+// let arr = [1,2,3,4,5];
+
+
+// let itr = arr[Symbol.iterator]();
+
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+
+
+
+// let obj = {
+//     name:"AJAY",
+//     age: 25, 
+//     isAdmin: true
+// }
+// Object.prototype[Symbol.iterator] = function(){
+//     return Object.keys(this)[Symbol.iterator]();
+// }
+
+
+// for(let prop of obj){
+//     console.log(prop);
+// }
+// let itr = obj[Symbol.iterator]();
+
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
+// console.log(itr.next());
