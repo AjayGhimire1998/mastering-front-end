0638054bca91d1187020becb39dbdb0492a5cfad
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-06 08:42:19 +1100

    gener

--- 4. JavaScript/Generators/index.js
+++ 4. JavaScript/Generators/index.js
@@ -1,6 +1,5 @@
 // let arr = [1,2,3,4,5];
 
-
 // let itr = arr[Symbol.iterator]();
 
 // console.log(itr.next());
@@ -10,17 +9,14 @@
 // console.log(itr.next());
 // console.log(itr.next());
 
-
-
 // let obj = {
 //     name:"AJAY",
-//     age: 25, 
+//     age: 25,
 //     isAdmin: true
 // }
 // Object.prototype[Symbol.iterator] = function(){
 //     return Object.keys(this)[Symbol.iterator]();
 // }
-
 
 // for(let prop of obj){
 //     console.log(prop);
@@ -31,3 +27,51 @@
 // console.log(itr.next());
 // console.log(itr.next());
 // console.log(itr.next());
+
+//generators
+
+function* generateSeq() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+
+let gen = generateSeq();
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+
+// for(let val of gen){
+//     console.log(val);
+// }
+
+// let seq = [0,1, ...gen]
+// console.log(seq);
+
+let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    return {
+      current: this.from,
+      last: this.to,
+      next() {
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        }
+        return { done: true };
+      },
+    };
+  },
+};
+
+let itr = range[Symbol.iterator]();
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log([...range, 6, 7, 8]);
