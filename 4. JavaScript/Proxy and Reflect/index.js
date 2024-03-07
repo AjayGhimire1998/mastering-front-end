33dcb4babedba0d3198fc4f0905d20279517ee3b
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 12:43:49 +1100

    proxies

--- 4. JavaScript/Proxy and Reflect/index.js
+++ 4. JavaScript/Proxy and Reflect/index.js
@@ -1,16 +1,65 @@
+// let target = {};
+
+// let proxy = new Proxy(target, {}); //empty handler
+
+// proxy.test = 5;
+
+// console.log(proxy);
+// console.log(target);
+
+// for(let key in proxy){
+//     console.log(key);
+// }
+
+// let arr = [1, 2, 3];
+// // console.log(arr[4]); //undefined
+
+// arr = new Proxy(arr, {
+//   get(target, prop) {
+//     if (prop in target) {
+//       return target[prop];
+//     } else {
+//       return 0;
+//     }
+//   },
+// });
+
+// console.log(arr[6]); //0
+
+//get trap
+// let dictionary = {
+//   Hello: "Hola",
+//   Bye: "Adiós",
+// };
+
+// dictionary = new Proxy(dictionary, {
+//   get(target, prop) {
+//     if (prop in target) {
+//       return target[prop];
+//     } else {
+//       return prop;
+//     }
+//   },
+// });
+
+// console.log(dictionary["SUPP"]);
+
+//set trap
+
+// let numbers = [];
+
+// numbers = new Proxy(numbers, {
+//   set(target, prop, val) {
+//     if (typeof val === "number") {
+//       target[prop] = val;
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   },
+// });
 
 
-let target = {}; 
-
-let proxy = new Proxy(target, {}); //empty handler
-
-
-proxy.test = 5;
-
-console.log(proxy);
-console.log(target);
-
-
-for(let key in proxy){
-    console.log(key);
-}
+// numbers.push(1);
+// numbers.push(2);
+// console.log(numbers);
