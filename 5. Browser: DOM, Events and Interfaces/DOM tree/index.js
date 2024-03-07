464d4acc521da2a67d3b763b4c11e3778840d68b
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 18:34:25 +1100

    dom

---
+++ 5. Browser: DOM, Events and Interfaces/DOM tree/index.js
@@ -0,0 +1,26 @@
+// function sayHi() {
+//   alert("Hello");
+// }
+
+// // global functions are methods of the global object:
+// window.sayHi();
+
+// alert(window.innerWidth)
+
+// document.body.style.backgroundColor = "purple";
+// setTimeout(() => document.body.style.background = "", 2000)
+
+// // alert(navigator.userAgent)
+// // alert(navigator.platform)
+// alert(location.href)
+
+
+// alert(location.href); // shows current URL
+// if (confirm("Go to Wikipedia?")) {
+//   location.href = "https://wikipedia.org"; // redirect the browser to another URL
+// }
+
+document.body.style.background = 'red'; // make the background red
+
+setTimeout(() => document.body.style.background = '', 3000); // return back
+
