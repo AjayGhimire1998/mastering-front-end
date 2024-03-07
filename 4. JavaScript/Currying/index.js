aae1431780cdda55925d3f186c4181dfccbd0c95
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 16:20:41 +1100

    curry

---
+++ 4. JavaScript/Currying/index.js
@@ -0,0 +1,15 @@
+// function curry(f) {
+//   return function (a) {
+//     return function (b) {
+//       return f(a, b);
+//     };
+//   };
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// let curried = curry(sum);
+
+// console.log(curried(1)(2));
