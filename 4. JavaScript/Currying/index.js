82b0d3c08735c8db6a7d1c73defce7488513b8d9
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 16:27:02 +1100

    curried advanced

--- 4. JavaScript/Currying/index.js
+++ 4. JavaScript/Currying/index.js
@@ -13,3 +13,31 @@
 // let curried = curry(sum);
 
 // console.log(curried(1)(2));
+
+// function log(date, importance, message) {
+//   console.log(
+//     `[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`
+//   );
+// }
+
+// function curry(func) {
+//   return function curried(...args) {
+//     if (args.length >= func.length) {
+//       return func.apply(this, args);
+//     } else {
+//       return function (...args2) {
+//         return curried.apply(this, args.concat(args2));
+//       };
+//     }
+//   };
+// }
+
+// function sum(a, b, c) {
+//     return a + b + c;
+//   }
+  
+//   let curriedSum = curry(sum);
+  
+//   console.log( curriedSum(1, 2, 3) ); // 6, still callable normally
+//   console.log( curriedSum(1)(2,3) ); // 6, currying of 1st arg
+//   console.log( curriedSum(1)(2)(3) ); 
