9f6426a69a4f3da78ba4cc941a50a3fc011a7906
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 12:58:32 +1100

    delete private prop

--- 4. JavaScript/Proxy and Reflect/index.js
+++ 4. JavaScript/Proxy and Reflect/index.js
@@ -80,4 +80,80 @@
 // }
 // console.log(Object.getOwnPropertyDescriptor(user, "name"))
 
+//delet property trap
 
+// let user = {
+//   name: "ajay",
+//   age: 25,
+//   _password: "***",
+//   checkPassword(value) {
+//     // object method must be able to read _password
+//     return value === this._password;
+//   },
+// };
+
+// user = new Proxy(user, {
+//   get(target, prop) {
+//     if (prop.startsWith("_")) {
+//       throw new Error("Access Denied!");
+//     }
+
+//     let value = target[prop];
+//     return typeof value === "function" ? value.bind(target) : value;
+//   },
+
+//   set(target, prop, val) {
+//     if (prop.startsWith("_")) {
+//       throw new Error("Access Denied!");
+//     } else {
+//       target[prop] = val;
+//       return true;
+//     }
+//   },
+
+//   deleteProperty(target, prop) {
+//     if (prop.startsWith("_")) {
+//       throw new Error("Access Denied!");
+//     } else {
+//       delete target[prop];
+//       return true;
+//     }
+//   },
+
+//   ownKeys(target) {
+//     return Object.keys(target).filter((key) => !key.startsWith("_"));
+//   },
+// });
+
+// // "get" doesn't allow to read _password
+// try {
+//   console.log(user._password); // Error: Access denied
+// } catch (e) {
+//   console.log(e.message);
+// }
+
+// // "set" doesn't allow to write _password
+// try {
+//   user._password = "test"; // Error: Access denied
+// } catch (e) {
+//   console.log(e.message);
+// }
+
+// try {
+//   delete user._password; // Error: Access denied
+// } catch (e) {
+//   console.log(e.message);
+// }
+
+// // "ownKeys" filters out _password
+// for (let key in user) console.log(key); // name
+
+// console.log(user.checkPassword("**"));
+
+
+//inrange with has trap
+
+let range = {
+    start: 1,
+    end: 10
+  };
