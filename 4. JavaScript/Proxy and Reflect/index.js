b3bdefc2e7b84eaae7f1ff4cec49fbaa10928a4c
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-07 12:50:12 +1100

    proxies to keys

--- 4. JavaScript/Proxy and Reflect/index.js
+++ 4. JavaScript/Proxy and Reflect/index.js
@@ -59,7 +59,25 @@
 //   },
 // });
 
-
 // numbers.push(1);
 // numbers.push(2);
 // console.log(numbers);
+
+// let user = {
+//   name: "ajay",
+//   age: 25,
+//   _password: "***",
+// };
+
+// user = new Proxy(user, {
+//   ownKeys(target) {
+//     return Object.keys(target).filter((key) => !key.startsWith("_"));
+//   },
+// });
+
+// for(let key in user){
+//     console.log(key);
+// }
+// console.log(Object.getOwnPropertyDescriptor(user, "name"))
+
+
