c492ae1ed635243b0d24800f646df2db25aac2d4
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-10 13:28:43 +1100

    modifying dom

--- 5. Browser: DOM, Events and Interfaces/Attributes and Properties/index.js
+++ 5. Browser: DOM, Events and Interfaces/Attributes and Properties/index.js
@@ -1,1 +1,21 @@
+// document.body.myData = {
+//   name: "Caesar",
+//   title: "Imperator",
+// };
 
+// alert(document.body.myData.title);
+
+// document.body.sayTagName = function () {
+//   alert(this.tagName);
+// };
+
+// document.body.sayTagName();
+
+// Element.prototype.sayHi = function () {
+//   alert(`Hello, I'm ${this.tagName}`);
+// };
+
+// document.documentElement.sayHi(); // Hello, I'm HTML
+// document.body.sayHi(); // Hello, I'm BODY
+
+// alert(document.body.dataset); 
---
+++ 5. Browser: DOM, Events and Interfaces/Modifyiing DOM/index.js
@@ -0,0 +1,2 @@
+let div = document.createElement("div");
+let textNode = document.createTextNode("Here I am");
