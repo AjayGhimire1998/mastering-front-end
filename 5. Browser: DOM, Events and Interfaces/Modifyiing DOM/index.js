007204d655f3efd4cfced0cb41f527b7cf9783bc
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-10 14:09:39 +1100

    Addimg li

--- 5. Browser: DOM, Events and Interfaces/Modifyiing DOM/index.js
+++ 5. Browser: DOM, Events and Interfaces/Modifyiing DOM/index.js
@@ -1,26 +1,25 @@
 let div = document.createElement("div");
 let textNode = document.createTextNode("Here I am");
 
-div.setAttribute("class", "alert");
-div.setAttribute("id", "alerId");
+// div.setAttribute("class", "alert");
+// div.setAttribute("id", "alerId");
 
-div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+// div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
 
 // document.body.append(div);
-console.log(div.attributes);
+// console.log(div.attributes);
 
-ol.before("before");
-ol.after("after");
+// ol.before("before");
+// ol.after("after");
 
-let liFirst = document.createElement("li");
-liFirst.innerHTML = "prepend";
-ol.prepend(liFirst);
+// let liFirst = document.createElement("li");
+// liFirst.innerHTML = "prepend";
+// ol.prepend(liFirst);
 
-let liLast = document.createElement("li");
-liLast.innerHTML = "append";
-ol.append(liLast);
+// let liLast = document.createElement("li");
+// liLast.innerHTML = "append";
+// ol.append(liLast);
 
-// // document.append('<br>')
 // div.before(
 //   document.createElement("br"),
 //   "<p>Hello</p>",
@@ -31,5 +30,10 @@ ol.append(liLast);
 // div.insertAdjacentHTML("beforebegin", "<p>Hello</p>");
 // div.insertAdjacentHTML("afterend", "<p>Bye</p>");
 
-setTimeout(() => div.remove(), 1000);
-second.after(first); 
+// // setTimeout(() => div.remove(), 1000);
+// // second.after(first);
+
+// let div2 = div.cloneNode(true); // clone the message
+// div2.querySelector("strong").innerHTML = "Bye there!"; // change the clone
+
+// div.after(div2);
