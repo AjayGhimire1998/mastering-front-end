dfc7c501471e58523453e68e8dbe9fbd518906d3
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-14 07:34:11 +1100

    text decoder

--- 4. JavaScript/ArrayBuffers or Binary Arrays/index.js
+++ 4. JavaScript/ArrayBuffers or Binary Arrays/index.js
@@ -1,17 +1,21 @@
-let buffer = new ArrayBuffer(16);
+// let buffer = new ArrayBuffer(16);
 // console.log(buffer.byteLength);
 
+// let view = new Uint32Array(buffer);
+// // console.log(view.length);
 
-let view = new Uint32Array(buffer);
-// console.log(view.length);
+// // console.log(Uint32Array.BYTES_PER_ELEMENT);
 
-// console.log(Uint32Array.BYTES_PER_ELEMENT);
+// // console.log(view.byteLength);
 
-// console.log(view.byteLength);
+// view[2] = 123245;
 
+// for (num of view) {
+//   console.log(num);
+// }
 
-view[0] = 123245;
+//text encode and decoder
 
-for(num of view){
-    console.log(num);
-}
+let decoder = new TextDecoder();
+let uint8Arr = new Uint8Array([72, 101, 108, 108, 111]);
+console.log(decoder.decode(uint8Arr)); //Hello
