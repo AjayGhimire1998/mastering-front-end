11171f48981be899ac3473b4e5a6a7a4f17e3034
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-17 19:44:29 +1100

    url obj

--- 4. JavaScript/URL/index.js
+++ 4. JavaScript/URL/index.js
@@ -1,1 +1,13 @@
 let url = new URL('https://google.com/search');
+
+
+
+url.searchParams.set('q', 'test me!');
+console.log(url);
+
+url.searchParams.set('tbs', 'qdr:y'); // 
+console.log(url);
+
+for(let [name, value] of url.searchParams) {
+    console.log(`${name}=${value}`); // q=test me!, then tbs=qdr:y
+  }
