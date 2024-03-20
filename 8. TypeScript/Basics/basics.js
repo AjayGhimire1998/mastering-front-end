8db6e5e2523582abdda5494d1675e1e434112b8a
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-20 20:08:43 +1100

    changes

--- 8. TypeScript/Basics/basics.js
+++ 8. TypeScript/Basics/basics.js
@@ -1,13 +1,30 @@
-// var num = 5;
+// let num: number = 5;
 // console.log(num);
-// var User = /** @class */ (function () {
-//     function User(name) {
-//         this.name = name;
-//     }
-//     return User;
-// }());
-// var user = new User("ajay");
+// type Person = {
+//   name: string;
+// };
+// class User implements Person {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// let user = new User("ajay");
 // console.log(user);
-
-
-
+// const message = "Hello World!";
+// message.toLowerCase();
+// let isWorldSaved: Boolean = false;
+// export function saveTheWorld(): String {
+//   if (isWorldSaved) {
+//     return `Too late, world has already been saved`;
+//   } else {
+//     isWorldSaved = true;
+//     return `Hurray, you just saved the world`;
+//   }
+// }
+// const message: Array<string> = ["Hello", "World"];
+// message.forEach((msg: string) => console.log(msg));
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan", new Date());
--- 8. TypeScript/Basics/basics.ts
+++ 8. TypeScript/Basics/basics.ts
@@ -1,16 +1,38 @@
-let num: number = 5;
-console.log(num);
+// let num: number = 5;
+// console.log(num);
 
-type Person = {
-  name: string;
-};
+// type Person = {
+//   name: string;
+// };
 
-class User implements Person {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
+// class User implements Person {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+
+// let user = new User("ajay");
+// console.log(user);
+// const message = "Hello World!";
+// message.toLowerCase();
+
+// let isWorldSaved: Boolean = false;
+
+// export function saveTheWorld(): String {
+//   if (isWorldSaved) {
+//     return `Too late, world has already been saved`;
+//   } else {
+//     isWorldSaved = true;
+//     return `Hurray, you just saved the world`;
+//   }
+// }
+
+// const message: Array<string> = ["Hello", "World"];
+// message.forEach((msg: string) => console.log(msg));
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date}!`);
 }
-
-let user = new User("ajay");
-console.log(user);
+ 
+greet("Brendan", new Date());
