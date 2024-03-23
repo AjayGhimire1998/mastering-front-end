a4cf0d8d84b70888bcc2c5c7870defbe83ff1730
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-24 10:02:47 +1100

    everyday types/functions

--- 8. TypeScript/Everyday Types/index.js
+++ 8. TypeScript/Everyday Types/index.js
@@ -14,7 +14,28 @@
 // }).then(() => {
 //     console.log(27);
 // })
-var names = ["ajay", "alice", "bob"];
-names.forEach(function (s) {
-    console.log(s.toUpperCase());
-});
+// const names: Array<string> = ["ajay", "alice", "bob"];
+// names.forEach((s: string) => {
+//   console.log(s.toUpperCase());
+// });
+// function printCoords(pt: { x: number; y: number }) {
+//   console.log("The coordinate's x value is " + pt.x);
+//   console.log("The coordinate's y value is " + pt.y);
+// }
+// printCoords({ x: 3, y: 7 });
+// function printName(obj: { first: string; last?: string }) {
+//   console.log(obj.first + obj.last);
+// }
+// function printName(obj: { first: string; last?: string }) {
+//   // Error - might crash if 'obj.last' wasn't provided!
+//   console.log(obj.last?.toUpperCase());
+//   if (obj.last !== undefined) {
+//     // OK
+//     console.log(obj.last.toUpperCase());
+//   }
+//   // A safe alternative using modern JavaScript syntax:
+//   console.log(obj.last?.toUpperCase());
+// }
+// printName({ first: "Bob" });
+// printName({ first: "Alice", last: "Alisson" });
+//union types
