e032a6963422b801d34ed90545fbd2a686fb794c
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-24 10:12:37 +1100

    everyday types/aliases

--- 8. TypeScript/Everyday Types/index.js
+++ 8. TypeScript/Everyday Types/index.js
@@ -45,15 +45,15 @@
 // printId("ajay123")
 // printId(234);
 // printId({id: 123})
-function welcomePeople(x) {
-    if (Array.isArray(x)) {
-        // Here: 'x' is 'string[]'
-        console.log("Hello, " + x.join(" and "));
-    }
-    else {
-        // Here: 'x' is 'string'
-        console.log("Welcome lone traveler " + x);
-    }
-}
-welcomePeople(["alice", "jhane"]);
-welcomePeople("ajay");
+// function welcomePeople(x: string[] | string) {
+//   if (Array.isArray(x)) {
+//     // Here: 'x' is 'string[]'
+//     console.log("Hello, " + x.join(" and "));
+//   } else {
+//     // Here: 'x' is 'string'
+//     console.log("Welcome lone traveler " + x);
+//   }
+// }
+// welcomePeople(["alice", "jhane"])
+// welcomePeople("ajay");
+//type aliases
--- 8. TypeScript/Everyday Types/index.ts
+++ 8. TypeScript/Everyday Types/index.ts
@@ -64,14 +64,17 @@
 // printId(234);
 // printId({id: 123})
 
-function welcomePeople(x: string[] | string) {
-  if (Array.isArray(x)) {
-    // Here: 'x' is 'string[]'
-    console.log("Hello, " + x.join(" and "));
-  } else {
-    // Here: 'x' is 'string'
-    console.log("Welcome lone traveler " + x);
-  }
-}
-welcomePeople(["alice", "jhane"])
-welcomePeople("ajay");
+// function welcomePeople(x: string[] | string) {
+//   if (Array.isArray(x)) {
+//     // Here: 'x' is 'string[]'
+//     console.log("Hello, " + x.join(" and "));
+//   } else {
+//     // Here: 'x' is 'string'
+//     console.log("Welcome lone traveler " + x);
+//   }
+// }
+// welcomePeople(["alice", "jhane"])
+// welcomePeople("ajay");
+
+
+//type aliases
