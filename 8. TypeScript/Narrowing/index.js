962882eb995735af0d5eacedbebec82b70c30342
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-24 12:24:13 +1100

    narrowing

--- 8. TypeScript/Narrowing/index.js
+++ 8. TypeScript/Narrowing/index.js
@@ -1,4 +1,23 @@
-function padLeft(padding, input) {
-    throw new Error("Not implemented yet!");
-}
-padLeft(12, "ok");
+// function padLeft(padding: number | string, input: string): string {
+//   if (typeof padding === "number") {
+//     return " ".repeat(padding) + input;
+//   }
+//   return padding + input;
+// }
+// console.log(padLeft(12, "ok"));
+// type guard
+// function printAll(strs: string | string[] | null) {
+//   if (typeof strs === "object") {
+//     for (const s of strs) {
+//       console.log(s);
+//     }
+//   } else if (typeof strs === "string") {
+//     console.log(strs);
+//   } else {
+//     // do nothing
+//   }
+// }
+// printAll(["ajay", "alice"])
+// console.log(typeof 245215686561389562379857823n);
+
+console.log(!!"hello")
--- 8. TypeScript/Narrowing/index.ts
+++ 8. TypeScript/Narrowing/index.ts
@@ -1,5 +1,28 @@
-function padLeft(padding: number | string, input: string): string {
-  throw new Error("Not implemented yet!");
-}
+// function padLeft(padding: number | string, input: string): string {
+//   if (typeof padding === "number") {
+//     return " ".repeat(padding) + input;
+//   }
 
-padLeft(12, "ok");
+//   return padding + input;
+// }
+
+// console.log(padLeft(12, "ok"));
+
+// type guard
+
+// function printAll(strs: string | string[] | null) {
+//   if (typeof strs === "object") {
+//     for (const s of strs) {
+//       console.log(s);
+//     }
+//   } else if (typeof strs === "string") {
+//     console.log(strs);
+//   } else {
+//     // do nothing
+//   }
+// }
+
+
+// printAll(["ajay", "alice"])
+
+console.log(typeof 245215686561389562379857823n);
