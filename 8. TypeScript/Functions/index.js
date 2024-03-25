dbe63e8ceb534ec73650e817e1a16c7115c0ab26
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-03-25 18:57:00 +1100

    functions

---
+++ 8. TypeScript/Functions/index.js
@@ -0,0 +1,13 @@
+//call signatures
+// type DescribableFunction = {
+//   description: string;
+//   (someArg: number): boolean;
+// };
+// function doSomething(fn: DescribableFunction) {
+//   console.log(fn.description + " returned " + fn(1));
+// }
+// function myFunc(someArg: number) {
+//   return someArg > 3;
+// }
+// myFunc.description = "default desc";
+// doSomething(myFunc);
